99f6a29445b72b9f00a1d26e6efbb9d3863cd936
jewelking88 <38009424+jewelking88@users.noreply.github.com>
2018-06-11 08:21:52 -0400

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,19 @@
+
+document.getElementById("topping1").addEventListener("change", function() {
+  document.getElementById("cheese").style.visibility="visible";
+})
+document.getElementById("topping2").addEventListener("change", function() {
+  document.getElementById("peperoni").style.visibility="visible";
+})
+document.getElementById("topping3").addEventListener("change", function() {
+  document.getElementById("pineapple").style.visibility="visible";
+})
+document.getElementById("topping4").addEventListener("change", function() {
+  document.getElementById("bacon").style.visibility="visible";
+})
+document.getElementById("topping5").addEventListener("change", function() {
+  document.getElementById("steak").style.visibility="visible";
+})
+document.getElementById("topping6").addEventListener("change", function() {
+  document.getElementById("pork").style.visibility="visible";
+})
